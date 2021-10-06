0da4bfdda937252f609f5a6af7740499a4b76e70
Andbenj <andbenj@student.chalmers.se>
2021-10-06 12:59:34 +0200

    Reciever file for COM B
    Reciever file for COM B
    it message for your changes. Lines starting

--- Chat_PC_B/receiver.m
+++ Chat_PC_B/receiver.m
@@ -13,12 +13,11 @@
 %%
 
 function [audio_recorder] = receiver(fc)
-fc = 3000;
-fs = 5000; %sampling frequency
+fs = 44000; %sampling frequency
 audio_recorder = audiorecorder(fs,24,1);% create the recorder
 
 %attach callback function
-time_value = 1; % how often the function should be called in seconds
+time_value = 6; % how often the function should be called in seconds
 set(audio_recorder,'TimerPeriod',time_value,'TimerFcn',@audioTimerFcn); % attach a function that should be called every second, the function that is called is specified below.
 
 %ADD USER DATA FOR CALLBACK FUNCTION (DO NOT CHANGE THE NAMES OF THESE VARIABLES!)
@@ -50,33 +49,124 @@ end
 % a = 432 bits
 % M = 4 (using QPSK as in computer exercise)
 
-function audioTimerFcn(recObj, event, handles)
+function audioTimerFcn(recObj, event, ~)
 
 %-----------------------------------------------------------
 % THE CODE BELOW IS BASED ON COMPUTER EX 5 AND EX 6:
 %-----------------------------------------------------------
-disp('Callback triggered')
+disp('Callback triggered')                                         % Number of samples per symbol (choose fs such that fsfd is an integer) [samples/symb]                          % create sinc pulse with span = 6
 
-fs = 5000;                                              % sampling frequency
-N = 432;                                                % number of bits
-const = [(1 + 1i) (1 - 1i) (-1 -1i) (-1 + 1i)]/sqrt(2); % Constellation 1 - QPSK/4-QAM
-M = length(const);                                      % Number of symbols in the constellation
-bpsymb = log2(M);                                       % Number of bits per symbol
-Rs = 500;                                                % Symbol rate [symb/s]
-Ts = 1/Rs;                                              % Symbol time [s/symb]
-fsfd = fs/Rs;                                           % Number of samples per symbol (choose fs such that fsfd is an integer) [samples/symb]
-bits = randsrc(1,N,[0 1]);                              % Information bits
-m_buffer = buffer(bits, bpsymb)';                       % Group bits into bits per symbol
-m = bi2de(m_buffer, 'left-msb')'+1;                     % Bits to symbol index
-x = const(m);                                           % Look up symbols using the indices
-x = awgn(x,15);                                          % add artificial noise
-x_upsample = upsample(x, fsfd);                         % Space the symbols fsfd apart, to enable pulse shaping using conv.
-span = 6;                                               % Set span = 6
-t_vec = -span*Ts: 1/fs :span*Ts;                        % create time vector for one sinc pulse
-pulse = sinc(t_vec/Ts);                                 % create sinc pulse with span = 6
-pulse_train = conv(pulse,x_upsample);                   % make pulse train
+constellation = [-1-1i, -1+1i, 1+1i, 1-1i];
+N = 432;
+fs = 44000;
+fc = 5000;
+Tsamp = 1/fs;
+M = length(constellation);
+bpsymb = log2(M);
+Rb = 440;
+fsymb = Rb/bpsymb;
+Tsamp = 1/fs;
+tau = 1/440; % 
+alpha = 0.35;
+span = 6;
+Tsymb = 1/fsymb;
+BW = (1+alpha)/(2*tau);
+fsfd = fs/fsymb;
+%preamble = [1 1 1 -1 -1 1 -1];     % Length-7 Barker code
+%disp('2')
+
+bq = [1 1 1 1 1 -1 -1 1 1 -1 1 -1 1];
+%cos(pi)
+%disp('3')
+bq = resample(bq, 100, 1);
+bq = bq.*cos(fc.*Tsamp.*(1:length(bq)));
+
+figure(40)
+plot(bq)
+%('1')
+y = rtrcpuls(alpha, Tsymb, fs, span);
+
+rx = getaudiodata(recObj).';
+
+%rx = rx/max(abs(rx));
+
+rx_temp = rx;
+figure(39)
+subplot(2,2,1)
+plot(rx)
+correl = xcorr(rx, bq);
+subplot(2,2,2)
+plot(correl)    
+[~, I] = max(correl);
+%disp(I)
+%disp(length(rx))
+rx = rx(I-length(correl)/2+1300:45560+I-length(correl)/2+1300);
+
+subplot(2,2,3)
+plot(rx)
+I_rx = rx.*cos(2.*pi.*fc.*Tsamp.*(0:length(rx)-1));
+Q_rx = 1i.*rx.*sin(2.*pi.*fc.*Tsamp.*(0:length(rx)-1));
+
+rx = lowpass(I_rx + Q_rx, fc/10, fs);
+
+MF = fliplr(conj(y));
+MF_out = conv(MF, rx);
+MF_out = MF_out(2*span*fs*Tsymb  :  end-2*span*fs*Tsymb);
+rx_vec = MF_out(1:fs*Tsymb:end);
+rx_vec = rx_vec/median(abs(rx_vec));
+subplot(2,2,4)
+plot(real(rx))
+%disp(rx_vec)
+% data_out = {};
+% for point = rx_vec
+%     temp = 10000;
+%     temp2 = 0;
+%     i = 1;
+%     for const = constellation
+%         if(abs(point - const) < temp)
+%             temp = abs(point - const);
+%             temp2 = i;
+%         end
+%         i = i + 1;
+%         
+%     end
+%     data_out = [data_out, temp2];
+% end
+
+rx_vec = transpose(rx_vec);
+constellation = transpose(constellation);
+
+const_rep = repmat(constellation, [1, length(rx_vec)]);
+
+[~, index] = min(transpose(abs(rx_vec - transpose(const_rep))));
+
+test = [0;1;1;0;0;1;0;0;0;1;1;0;0;0;0;1;0;1;1;1;0;0;1;1;0;1;1;0;1;0;1;1;0;1;1;0;0;1;0;0;0;1;1;0;1;1;1;1;0;1;1;0;0;0;0;1;0;1;1;1;0;0;1;1;0;1;1;0;1;0;1;0;0;1;1;0;0;1;0;0;0;1;1;0;1;0;1;0;0;1;1;0;0;0;0;1;0;1;1;0;1;0;0;1;0;1;1;1;0;0;1;1;0;1;1;0;0;1;0;0;0;1;1;0;1;0;1;0;0;1;1;1;0;0;1;1;0;1;1;0;0;0;0;1;0;1;1;0;1;1;0;1;0;1;1;0;0;1;0;0;0;1;1;0;1;0;1;1;0;1;1;0;0;0;0;1;0;1;1;1;0;0;1;1;0;1;1;0;1;0;1;0;0;1;1;0;0;1;0;0;0;1;1;0;1;0;1;1;0;1;1;0;1;0;0;1;0;1;1;0;0;0;0;1;0;1;1;0;1;0;1;0;0;1;1;0;0;1;0;0;0;1;1;0;1;0;0;1;0;1;1;0;1;0;1;0;0;1;1;0;0;0;0;1;0;1;1;0;0;1;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;1;1;1;1;1;0;1;1;1;0;0;1;0;0;1;0;1;1;0;1;1;1;1;1;1;0;1;1;1;0;0];
+%load('file.mat')
+%data_out = cell2mat(data_out)-1;
+
+%disp(sum(data_out))
 
 
+pulse_train = MF_out;
+x = rx_vec;
+%data_out = de2bi(data_out, 'left-msb');
+m = de2bi(index - 1, bpsymb, 'left-msb'); 
+data_out = reshape(m', N, 1);
+disp(size(test))
+disp(size(data_out))
+data_out = data_out(:).';
+disp(data_out)
+figure(592)
+subplot(2,1,1)
+plot(test)
+subplot(2,1,2)
+plot(data_out)
+if all(test == data_out.')
+    disp('ok!')
+else
+    disp('not ok')
+end
+%disp(length(data_out))
 
 %------------------------------------------------------------------------------
 % HOW TO SAVE DATA FOR THE GUI
@@ -84,7 +174,7 @@ pulse_train = conv(pulse,x_upsample);                   % make pulse train
 %------------------------------------------------------------------------------
 
 % Step 1: save the estimated bits
-recObj.UserData.pack = bits;
+recObj.UserData.pack = data_out;
 
 % Step 2: save the sampled symbols
 recObj.UserData.const = x;
